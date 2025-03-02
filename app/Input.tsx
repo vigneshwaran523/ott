be756033d33de4a28afd1feaa46800bc7ee67c94
vigneshwaran <vigneshwaran060704@gmail.com>
2025-03-02 21:09:03 +0530

    new

--- app/Input.tsx
+++ app/Input.tsx
@@ -1,8 +1,66 @@
-const Input =() => {
+import react from 'react';
+
+interface Inputprops {
+    id:string;
+    onchange:any;
+    value:string;
+    label:string;
+    type?:string;
+}
+
+const Input: react.FC<Inputprops> =({
+    id,
+    onchange,
+    value,
+    label,
+    type
+}) => {
     return(
-        <input className="
+     <div className=" relative "> 
+        <input
+        onChange={onchange}
+        type={type}
+        value={value}
+        id="id" 
+        className="
+        block
+        rounded-md
+        px-6
+        pt-6
+        pt-1
+        w-full
+        text-md
+        text-white
+        bg-neutral-700
+        appearance-none
+        focus:outline-none
+        focus:ring-0
+        peer
         "
+        placeholder=" "
         />
+        <label 
+        className="
+        absolute
+        text-md
+        text-zinc-400
+        duraytion-150
+        transform
+        -translate-y-3
+        scale-75
+        top-4
+        z-10
+        origin-[0]
+        left-6
+        peer-placeholder-shown:scale-100
+        peer-placeholder-shown:translate-y-0
+        peer-focus:scale-75
+        peer-focus:-translate-y-3
+        "
+        htmlFor="id">
+         {label}
+        </label>
+    </div>    
     )
 }
 
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,22 +1,49 @@
-import Input  from "../Input";
+'use client'
 
-const Auth = () => (
+import Input from "../Input";
+import { useState } from "react"; 
+
+
+
+const Auth = () => {
+    const [email,setemail] = useState(" ")
+    const [password,setpassword] = useState(" ")
+    const [name,setname] = useState(" ")
+    return(
     <div className="relative h-screen w-full bg-[url('/images/logo.jpeg')] bg-cover bg-center">
         
             <nav className="px-12 py-5">
                 <img src="/images/net.png" alt="net" className="h-20" />
             </nav>
         <div className="flex justify-center">
-        <div className="bg-black bg-opacity-60 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-lg w-full">
+        <div className="bg-black bg-opacity-10 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-lg w-full">
                <h2 className="text-white text-3xl mb-8 font-semibold"> 
                 sign in 
                </h2>
                <div className="flex flex-col gap-4">
-                 <Input  />
+               <Input
+                  label="email"
+                  onchange={(ev:any) => setname(ev.target.value)}
+                  id="email"
+                  value={" "}
+               />
+               <Input
+                  label="username"
+                  onchange={(ev:any) => setname(ev.target.value)}
+                  id="name"
+                  value={" "}
+               />
+               <Input
+                  label="password"
+                  onchange={(ev:any) => setname(ev.target.value)}
+                  id="password"
+                  value={" "}
+               />
                </div>
             </div>
         </div>
     </div>
-);
+    );
+};
 
 export default Auth;
